5945ba2000cde24874cbf9ca25d4927a5c25499c
Number2wo <112464904+Number2wo@users.noreply.github.com>
2024-01-02 21:11:06 -0500

    Game Loop

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //2
+
+        // Set window size to full screen
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game so that objects in the Game class can be rendered to the screen
+        setContentView(new Game(this));
+
+
+
     }
 }
